719b77b2b0d675c380e12599c0b0e6116d9ce885
DESKTOP-FIG594Q\김호준 <hojune961207@gmail.com>
2023-03-09 18:58:09 +0900

    수정

--- tt/src/ExTest2.java
+++ tt/src/ExTest2.java
@@ -5,17 +5,21 @@ public class ExTest2 {
 
 		int ar[][][] = new int[3][][];
 		int a[] = { 5, 8, 10 };
-
-		for (int i = 0; i < ar.length; i++) {
+			
+ 		for (int i = 0; i < ar.length; i++) {
 			ar[i] = new int[a[i]][20];
 
 		}
+		
+		int cnt = 1; // ar[1] = {00000}
+		int a1 = 0;
+		int class1 =0;
 
-		int cnt = 1;
-
-		for (int i = 0; i < ar.length; i++) {
-			for (int j = 0; j < ar[i].length; j++) {
-				for (int j2 = 0; j2 < ar[i][j].length; j2++) {
+		for (int i = 0; i < ar.length; i++) { // 3 회전 int[a[i]][20];  ar[1]  int[a[i]][20];ar[2] int[a[i]][20]; ar[3] int[a[i]][20];
+				a1++;
+			for (int j = 0; j < ar[i].length; j++) {	//  ar[1] = { 0. 0. 0. 0. 0.} ar[2}= { 0.0 .0 .0 .0. 0. 0. 0.} //5회전 . 8회전 , 10 회전 
+				class1++;
+				for (int j2 = 0; j2 < ar[i][j].length; j2++) { // 20 회전 
 					ar[i][j][j2] = cnt;
 					cnt++;
 				}
@@ -33,6 +37,8 @@ public class ExTest2 {
 			}
 
 		}
+		System.out.printf("\n< 학급 : 총%d학년 , 반 : 총 %d반 , 학생수: %d먕>" , a1 , class1 , cnt -1);
+		
 	}
 
 }
