8bcfa5b167a77b08ee0f8ba6907013b4df197b5a
DESKTOP-FIG594Q\김호준 <hojune961207@gmail.com>
2023-03-10 10:00:49 +0900

    수정본

--- tt/src/ExTest2.java
+++ tt/src/ExTest2.java
@@ -5,20 +5,20 @@ public class ExTest2 {
 
 		int ar[][][] = new int[3][][];
 		int a[] = { 5, 8, 10 };
+		int cnt = 1; // ar[1] = {00000}
+		
+		int class1 =0;
 			
  		for (int i = 0; i < ar.length; i++) {
 			ar[i] = new int[a[i]][20];
-
+			class1 += a[i];
 		}
 		
-		int cnt = 1; // ar[1] = {00000}
-		int a1 = 0;
-		int class1 =0;
 
 		for (int i = 0; i < ar.length; i++) { // 3 회전 int[a[i]][20];  ar[1]  int[a[i]][20];ar[2] int[a[i]][20]; ar[3] int[a[i]][20];
-				a1++;
+				
 			for (int j = 0; j < ar[i].length; j++) {	//  ar[1] = { 0. 0. 0. 0. 0.} ar[2}= { 0.0 .0 .0 .0. 0. 0. 0.} //5회전 . 8회전 , 10 회전 
-				class1++;
+//				class1++;
 				for (int j2 = 0; j2 < ar[i][j].length; j2++) { // 20 회전 
 					ar[i][j][j2] = cnt;
 					cnt++;
@@ -27,17 +27,17 @@ public class ExTest2 {
 		}
 
 		for (int i = 0; i < ar.length; i++) {
-			System.out.printf(" < %d학년 >\n", 3 - i);
+			System.out.printf(" < %d학년 >\n", ar.length - i);
 			for (int j = 0; j < ar[i].length; j++) {
-				System.out.printf("%d반 : ", j+1);
+				System.out.printf("%2d반 : ", j+1);
 				for (int j2 = 0; j2 < ar[i][j].length; j2++) {
-					System.out.printf(" %d 번", ar[i][j][j2]);
+					System.out.printf(" %3d 번", ar[i][j][j2]);
 				}
 				System.out.println();
 			}
 
 		}
-		System.out.printf("\n< 학급 : 총%d학년 , 반 : 총 %d반 , 학생수: %d먕>" , a1 , class1 , cnt -1);
+		System.out.printf("\n< 학급 : 총%d학년 , 반 : 총 %d반 , 학생수: %d명>" , ar.length , class1 , cnt -1);
 		
 	}
 
