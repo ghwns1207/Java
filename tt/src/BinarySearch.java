26f7775a44764a863f3cb8f5064bcb8ca4d5c1d1
DESKTOP-FIG594Q\김호준 <hojune961207@gmail.com>
2023-03-13 14:30:58 +0900

    점심 커밋

---
+++ tt/src/BinarySearch.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class BinarySearch {
+
+	static int search(int a, int[] selectionSort, int Lx, int Hx) {
+		int arLength = (Lx + Hx) / 2;
+
+		if(Lx > Hx) {
+			return -1;
+		}else if (a < selectionSort[arLength]) {
+			Hx = arLength - 1;
+		}else{
+			Lx = arLength + 1;
+		}
+			
+		if(a == selectionSort[arLength]) {
+			return arLength + 1;
+		}
+		
+		return search(a, selectionSort, Lx, Hx);
+
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int[]selectionSort = { 33,11,77,66,55,100,22,88,44,99 };
+		
+		Scanner sc = new Scanner(System.in);
+		
+	    for(int j=0,t; j < selectionSort.length-1; j++) {
+	         for(int p = j+1; p<selectionSort.length; p++) {
+	            if(selectionSort[j] > selectionSort[p]) {
+	               t = selectionSort[j];
+	               selectionSort[j] = selectionSort[p];
+	               selectionSort[p] = t;
+	            }
+	         }
+	    }
+		
+		System.out.print("검색 값 입력 : ");
+		int  a = sc.nextInt();
+		int t = search(a , selectionSort , 0 , selectionSort.length - 1);
+		if(t > 0) {
+			System.out.printf( "\n검색 값 %d는(은) selectionSort배열 %d 번째 위치에 존재합니다." , a , t);
+		}else {
+			 System.out.println("\n찾으려는 값이 존재하지 않습니다.");
+		}
+		
+	}
+
+}
--- tt/src/MethodEX4.java
+++ tt/src/MethodEX4.java
@@ -3,28 +3,43 @@ import java.util.Scanner;
 public class MethodEX4 {
 	//호출 순서
 	
-	static int sum(int t ,int min , int max) {
-//		if(min > max) return s;
+//	static int sum(int t ,int min , int max) {
+////		if(min > max) return s;
+////		
+////		System.out.printf("%d %s ",min , min < max ? "+" : '\n');				
+////		return sum((s + min) , min +1, max);
 //		
-//		System.out.printf("%d %s ",min , min < max ? "+" : '\n');				
-//		return sum((s + min) , min +1, max);
-		
-		if(min > max) return 0;
-		
-		t = sum( t, min, max - 1);
-		
-		System.out.printf("%d %s ", max , min  >  max   ? '\n' : "+");
-		t += max;
-		
-		return t ;
-		}
+//	
+//		}
 	
+//	static int sum( int min , int max , int firstMax) {
+//		// 역순 
+//		int tot; 
+//		if(min > max) return 0;
+//		
+//		tot = sum(min, max - 1, firstMax) + max; 
+//		System.out.printf("%d%s" ,max, max < firstMax ? " + " : '\n');
+//		return tot;
+//		}
+	// 배열 버전 
+	static int sum( int min , int[] max) {
+		// 역순 
+		int tot; 
+		int tmax;
+		if(min > max[0]) return 0;
+		tmax = max[0]--;
+		
+		tot = sum(min, max) + tmax; 
+		System.out.printf("%d%s" , tmax, tmax < max[1] ? " + " : '\n');
+		return tot;
+	}
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int a,b;
 		int min,max , tot = 0;
+		
 		System.out.print("정수 1 :");
 		a = sc.nextInt();
 		
@@ -46,8 +61,12 @@ public class MethodEX4 {
 		}
 		
 		System.out.printf( "<루프 이용 검증>%d부터 %d까지의 합은 : %d\n\n" , min, max, tot );
-		
-		System.out.printf("\n\n< 재귀 호출을 이용한 누적합> %d 부터 %d까지의 합은 : %d" ,min, max , sum(0, min , max));
+		//호출순
+//		System.out.printf("\n\n< 재귀 호출을 이용한 누적합> %d 부터 %d까지의 합은 : %d" ,min, max , sum(0, min , max));
+		//역순
+//		System.out.printf("\n\n< 재귀 호출을 이용한 누적합> %d 부터 %d까지의 합은 : %d" ,min, max , sum(min , max , max));
+		//배열 사용 역순
+		System.out.printf("\n\n< 재귀 호출을 이용한 누적합> %d 부터 %d까지의 합은 : %d" ,min, max , sum(min, new int[]{max , max } ));
 	}
 
 }
